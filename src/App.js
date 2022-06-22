9381ec4beadbb16c936f4cf0547ce48a598aa469
SavaCosmin <cosmin.savaionut@gmail.com>
2022-06-22 15:27:48 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Support from './components/Support'
+import AllInOne from './components/AllInOne'
+import Pricing from './components/Pricing'
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support/>
+      <AllInOne />
+      <Pricing />
+    </>
+  );
+}
+
+export default App;
